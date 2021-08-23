32ffb7cbf50a8f38bbec6739be9e7f585b3460a9
S.Tidjane <stidjane@outlook.com>
2021-08-23 03:21:03 +0200

    pushNight

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react" ;
+import "./App.css";
+import Form from "./components/Form" ;
+import Footer from "./components/Footer" ;
+import StepTwo from "./components/StepTwo" ;
+
 
 function App() {
+  const [confirmpass, setConfirmpass ] = useState("") ;
+  const [name, setName ] = useState("") ;
+  const [email, setEmail ] = useState("") ;
+  const [password, setPassword ] = useState("") ;  
+
+ const [ step, setStep] = useState(1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    { step === 1 ? (
+       
+        <Form name={name} setName={setName} 
+                  email={email} setEmail={setEmail} 
+                  password={password} setPassword={setPassword}
+                  confirmpass={confirmpass} setConfirmpass={setConfirmpass}
+                  step={step} setStep={setStep}  
+            />
+    ):(
+      
+        <StepTwo 
+        name={name} 
+        email={email} 
+        password={password} 
+        setStep={setStep} />
+
+    ) }
+    <div>    <Footer/>   </div>
+    </>
   );
 }
 
